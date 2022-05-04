9f9fd3db5878d15619270ad8eb27cbbd84395c4d
Daisy-McG <mcgirr.daisy@gmail.com>
2022-05-05 00:32:36 +0100

    feat: add client to admin panel

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        'client',
+        'address_line_one',
+        'address_line_two',
+        'county',
+        'country',
+        'paid_to_date',
+        'balance',
+        'email',
+    )
+
+admin.site.register(Client, ClientAdmin)
