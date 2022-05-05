5508138971d1d4486ff14129346369b33e87dfc8
Daisy-McG <mcgirr.daisy@gmail.com>
2022-05-05 20:36:26 +0100

    test: add unit test for clients

--- clients/tests.py
+++ clients/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestViews(TestCase):
+    def test_clients_page_redirects_when_not_logged_in(self):
+        """ Test that the clients page redirects when user is not logged in """
+        response = self.client.get('/clients/')
+        self.assertEqual(response.status_code, 302)
+        # self.assertTemplateUsed(response, 'clients/view_clients.html')
+    
