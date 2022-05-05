46e0d07df04982c10d4d4e3b005a2e1816980130
Daisy-McG <mcgirr.daisy@gmail.com>
2022-05-05 19:50:37 +0100

    fix: add town to client model

--- clients/models.py
+++ clients/models.py
@@ -10,6 +10,7 @@ class Client(models.Model):
     client = models.CharField(max_length=200, null=False, blank=False)
     address_line_one = models.CharField(max_length=200, null=False, blank=False)
     address_line_two = models.CharField(max_length=200, null=True, blank=True)
+    town = models.CharField(max_length=200, null=True, blank=True)
     county = models.CharField(max_length=200, null=False, blank=False)
     country = models.CharField(max_length=200, null=False, blank=False)
     paid_to_date = models.DecimalField(max_digits=20, decimal_places=2)
